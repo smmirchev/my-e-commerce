e3491bf7d25e8d534799c437fbd6fe1fe72f1140
smmirchev <sm.mirchev@gmail.com>
2021-07-27 13:40:23 +0100

    temp change

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>The page you tried to load does not exist on the server.</p>
+    <p>The page you tried to load does not exist on the server. temp</p>
   </Layout>
 )
 
